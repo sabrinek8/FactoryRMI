4a683b2be3b5bb44fcfe6a5ecd56e2d13d72df80
Sabrine Kammoun <sabrinkammoun@gmail.com>
2024-03-16 00:49:21 +0100

    first commit

---
+++ FactoryServer/src/ReverseServer.java
@@ -0,0 +1,19 @@
+import java.rmi.*;
+import java.rmi.registry.*;
+
+public class ReverseServer {
+    public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.createRegistry(1099);
+            System.out.println("Serveur : Construction de l'implémentation");
+            FabReverseImpl fab = new FabReverseImpl();
+            System.out.println("Objet Fabrique lié dans le RMIregistry");
+            registry.rebind("Fabrique", fab);
+            System.out.println("Serveur prêt.");
+            System.out.println("Attente des invocations des clients...");
+        } catch (Exception e) {
+            System.out.println("Erreur de liaison de l'objet Fabrique");
+            System.out.println(e.toString());
+        }
+    }
+}
